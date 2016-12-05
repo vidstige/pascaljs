f54440244771511f649fdbd3fe48592fb42b96d6
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-12-05 21:18:32 +0100

    Refactor: Test harness now asserts on exact output

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 var fs = require('fs');
+var path = require('path');
 var parser = require('./pascal.js')
 
-function verify(filename) {
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            throw err; 
-        }
-        parser.parse(data.toString());
-    });
+function run(pascal_source) {
+    var stdout = '';
+    var original = console.log;
+    console.log = function (msg) { stdout += msg + "\n"; };
+    parser.parse(pascal_source);
+    console.log = original;
+    return {'stdout': stdout, 'stderr': null};
 }
 
-const testFolder = 'tests/';
-fs.readdir(testFolder, (err, files) => {
-  if (err) {
-      throw err;
-  }
-  files.forEach(file => {
-    console.log(file + "...");
-    verify(testFolder + file);
-  });
-})
+function verify(filepath) {
+    var data = fs.readFileSync(filepath, "utf8");
+    x = run(data.toString());
 
+    expected_stdout = path.dirname(filepath) + '/expectations/' + path.basename(filepath, '.pas') + ".out";
+    if (fs.existsSync(expected_stdout)) {
+        var expected = fs.readFileSync(expected_stdout, "utf8");
+        if (expected != x.stdout) {
+            console.error("Output missmatch! " + filepath);
+            console.error("expected:\n" + expected);
+            console.error("actual:\n" + x.stdout);
+        }
+    }
+}
+
+function verifyAll() {
+    const testFolder = 'tests/';
+    files = fs.readdirSync(testFolder); // sync because of console.log capture
+    for (var i = 0; i < files.length; i++) {
+        var path = testFolder + files[i];
+        if (!fs.lstatSync(path).isDirectory())
+        {
+            console.log(files[i] + "...");
+            verify(path);
+        }
+    }
+}
+verifyAll();
