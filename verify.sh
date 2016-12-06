cf6e9dbedbdcc41f8e0db3c893d5dc7430fc9655
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-12-06 21:16:54 +0100

    Refactor: Better reporting

--- verify.sh
+++ verify.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+function fancy {
+    if [ $1 -eq 0 ]; then
+        echo "✓ " $2
+    else
+        echo "✗ " $2
+    fi 
+}
+
+fails=0
 for obj in $1; do
   tmp="${obj/build/tests/expectations}"
   expected="${tmp%.js}.out"
-  echo `basename ${obj%.js}`...
+  test_name=`basename ${obj%.js}`
   if [ -f $expected ]; then
     node $obj | diff $expected -
-
   else
     node $obj > /dev/null
   fi
+  success=$?
+  if [ ! $success -eq 0 ]; then
+    ((fails++))
+  fi
+  fancy $success $test_name
 done
+exit $fails
