45a7725d15953f33d0c79477fcae17798893f2bd
vidstige <samuel.carlsson@gmail.com>
2016-12-12 13:13:40 +0100

    Node errors now blow up tests

--- verify.sh
+++ verify.sh
@@ -13,11 +13,14 @@ for obj in $1; do
   expected="${tmp%.js}.out"
   test_name=`basename ${obj%.js}`
   if [ -f $expected ]; then
-    node $obj | diff $expected -
+    actual=$(mktemp /tmp/pascaljs.XXXXXX)
+    node $obj > $actual && diff $actual $expected
+    success=$?
+    rm "$actual"
   else
     node $obj > /dev/null
+    success=$?
   fi
-  success=$?
   if [ ! $success -eq 0 ]; then
     ((fails++))
   fi
