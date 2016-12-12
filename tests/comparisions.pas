acbbc72c88a515e3dc97ec8e253a26cf0412c7e1
vidstige <samuel.carlsson@gmail.com>
2016-12-12 15:02:44 +0100

    Comparision operators

---
+++ tests/comparisions.pas
@@ -0,0 +1,15 @@
+program comparisions;
+var
+    x: Integer;
+    y: Integer;
+    z: Boolean;
+begin
+    x := 17;
+    y := 99;
+    WriteLn('x = y  ', x=y);
+    WriteLn('x >= y ', x>=y);
+    WriteLn('x <= y ', x<=y);
+    WriteLn('x <> y ', x<>y);
+    WriteLn('x > y  ', x>y);
+    WriteLn('x < y  ', x<y);
+end.
