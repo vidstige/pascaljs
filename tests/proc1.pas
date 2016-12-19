9a919a694895349103c2e1db2baef2de90c8f1ab
Samuel Carlsson <samuel.carlsson@gmail.com>
2016-12-19 22:18:53 +0100

    Refactor: Param-less proc test
    - Adding test case for procedure without parameters.

--- tests/proc1.pas
+++ tests/proc1.pas
@@ -1,8 +1,14 @@
 program Sample;
+procedure NoParameter();
+begin
+    WriteLn('No parameter'); 
+end;
+
 procedure Output(A,B: Integer; C: String);
 begin
     WriteLn(A, ', ', B, ', ', C); 
 end;
 begin
   Output(7, 8, 'C');
+  NoParameter();
 end.
