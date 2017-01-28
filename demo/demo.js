0332fb6da97877832da8cdf23487337b42c5c902
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-01-28 19:18:38 +0100

    Now also running the Pascal program

--- demo/demo.js
+++ demo/demo.js
@@ -6,14 +6,29 @@ function run(source_id, output_id) {
     console.log('id: #' + source_id);
     console.log('id: #' + output_id);
 
+    var object_code = "";
     var raw_emit = function (line) {
-        document.getElementById(output_id).innerHTML += line + '<br/>\n';
+        object_code += line + "\n";
     };
 
     var pascal_source = document.getElementById(source_id).value;
     var ast = parser.parse(pascal_source);
     var e = new emitter.Emitter(raw_emit);
     e.emit(ast);
+
+    // TODO: Special variant of WriteLn that does not write to console.log
+    var original = console.log;
+    var output = "";    
+    console.log = function (line) {
+        output += line;
+    }
+
+    // Run js object code
+    eval(object_code);
+
+    console.log = original;
+
+    document.getElementById(output_id).innerHTML = output;
 }
 
 window.demo = { run: run }
