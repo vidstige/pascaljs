55f866062650fa2c071fe3ef025f18ce20eda09d
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-06-20 20:45:26 +0200

    trailing semi in records

--- tests/record1.pas
+++ tests/record1.pas
@@ -1,13 +1,22 @@
 programrecord1;
 type
+    { trailing semi}
     TPoint = record
-        x: Integer;
-        y: Integer
+        x, y: Integer;
+    end;
+    { no trailing semi}
+    TName = record
+        first: String;
+        last: String
     end;
 var
     p: TPoint;
+    name: TName;
 begin
     p.x := 1;
     p.y := 2;
     WriteLn('x: ', p.x, ', y: ', p.y);
+    name.first := 'Peter';
+    name.last := 'Forsberg';
+    WriteLn(name.first, ' ', name.last);
 end.
