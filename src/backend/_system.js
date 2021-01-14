da48b3125f45fc01e405afedcc4ad48420b22d57
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-01-14 21:25:23 +0100

    refactor: inline local

--- src/backend/_system.js
+++ src/backend/_system.js
@@ -1,1 +1,1 @@
-function WriteLn() { var args = Array.prototype.slice.call(arguments); console.log(args.join('')); }
+function WriteLn() { console.log(Array.prototype.slice.call(arguments).join('')); }
