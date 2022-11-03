a6ab4fe5fb11508d232a43d96a43e4c55a6599ed
Samuel Carlsson <samuel.carlsson@gmail.com>
2022-11-03 12:04:24 +0100

    switchto simpler algorithm

--- src/backend/assembler.js
+++ src/backend/assembler.js
@@ -1,78 +1,22 @@
 // https://dl.acm.org/doi/pdf/10.1145/3547621
 // https://en.wikipedia.org/wiki/Dominator_(graph_theory)
 
-// returns a set containing the elements that are in all sets
-function intersection(sets) {
-  var everything = new Set();
-  for (var set of sets) {
-    for (var element of set) {
-      everything.add(element);
-    }
-  }
-
-  var rm = [];
-  for (var element of everything) {
-    for (var set of sets) {
-      if (!set.has(element)) {
-        rm.push(element);
-        break;
-      }
-    }
-  }
-  for (var element in rm) {
-    everything.remove(element);
-  }
-  return everything;
-}
-
 function isBranch(statement) {
-  return statement.mnemonic == 'loop';
+  const branchMnemonics = ['loop', 'je', 'jne'];
+  return branchMnemonics.includes(statement.mnemonic);
 }
 
-function findPrevious(leaders, node) {
-  return Math.min.apply(null, leaders.filter(leader => leader < node));
-}
-
-function get_parents(node, edges) {
-  return edges.filter(edge => edge.target == node).map(edge => edge.source);
-}
-
-function find_dominators(nodes, edges) {
-  const start = 0
-  var dom = {};
-  for (var node of nodes) {
-    if (node == start) {
-      // dominator of the start node is the start itself
-      dom[node] = new Set([node]);
-    } else {
-      // for all other nodes, set all nodes as the dominators
-      dom[node] = new Set(nodes);
-    }
-  }
-
-  // iteratively eliminate nodes that are not dominators
-  var changed = true;
-  while (changed) { // changes in any Dom(n)
-    changed = false;
-    for (var node of nodes) {
-      if (node == start) continue; // skip start node
-      // samuel: what's predecesors? Must be parents in the cfg, right?
-      var domp = [];
-      for (var parent of get_parents(node, edges)) {
-        domp.push(dom[parent]);
-      }
-      var newValue = intersection(domp);
-      newValue.add(node);
-      changed |= newValue == dom[node];
-      dom[node] = newValue;
-      //Dom(n) = {n} union with intersection over Dom(p) for all p in pred(n)
-    }
-  }
-  return dom;
+function findNode(leaders, index) {
+  return Math.min.apply(null, leaders.filter(leader => leader < index));
 }
 
 class ControlFlowGraph {
-  constructor(statements) {
+  constructor(nodes, edges) {
+    this.start = 0;
+    this.nodes = nodes;
+    this.edges = edges;
+  }
+  static build(statements) {
     // 1. Create label lookup
     var labels = {};
     for (var i = 0; i < statements.length; i++) {
@@ -87,42 +31,101 @@ class ControlFlowGraph {
     var leaders = [0]; // first instruction is always leader
     for (var i = 0; i < statements.length; i++) {
       const statement = statements[i];
+      const node = findNode(leaders, i);
       if (isBranch(statement)) {
-        // link back branch target with previous block
-        const previous = findPrevious(leaders, i);
-        edges.push({source: previous, target: labels[statement.to]});
-        
         leaders.push(labels[statement.to]);  // branch target is a leader
-        edges.push({source: i, target: labels[statement.to]});
+        edges.push({source: node, target: labels[statement.to]});
 
-        leaders.push(i + 1); // instruction after loop is also a leader
-        edges.push({source: i, target: i + 1});
+        leaders.push(i + 1); // instruction after branch is also a leader
+        edges.push({source: node, target: i + 1});
       }
     }
-    // Add previous => exit if needed
-    const previous = findPrevious(leaders, statements.length - 1);
     const exit = statements.length;
+    // Add previous => exit if needed
+    /*const previous = findPrevious(leaders, statements.length - 1);
     if (!edges.includes({source: previous, target: exit})) {
       edges.push({source: previous, target: exit});
-    }
+    }*/
     
-    leaders.push(exit);
+    //leaders.push(exit);
     leaders.sort(function (a, b) { return a - b; });
 
-    // 3. Compute Immediate Dominator Tree (DomT)
-    const dominators = find_dominators(leaders, edges);
-    console.error(dominators);
-
-    this.leaders = leaders;
-    this.edges = edges;
-    this.statements = statements;
+    return new ControlFlowGraph(leaders, edges);
   }
-  reduce() {
-    // converts assembler to intermediate strucutured flow 
-  } 
+  postOrder() {
+    // returns the nodes in post-order (dfs)
+    var order = [];
+    var queue = [this.start];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      // visit all childs
+      for (var child of this.childs(node)) {
+        if (!order.includes(child)) {
+          order.push(child);
+        }
+        queue.push(child);
+      }
+      // visit myself
+      if (!order.includes(node)) {
+        order.push(node);
+      }
+    }
+    if (order.length != this.nodes.length) throw "Post order";
+    return order;
+  }
+  childs(node) {
+    return this.edges.filter(edge => edge.source == node).map(edge => edge.target);
+  }
+  remove(node) {
+    return new ControlFlowGraph(
+      this.nodes.filter(n => n != node),
+      this.edges.filter(edge => edge.target != node),
+    );
+  }
+}
+
+function reachable(cfg) {
+  const visited = new Set();
+  if (!cfg.nodes.includes(cfg.start)) {
+    return visited;
+  }
+  var queue = [cfg.start];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    visited.add(node);
+    for (var child of cfg.childs(node)) {
+      if (!visited.has(child)) {
+        queue.push(child);
+      }
+    }
+  }
+  return visited;
+}
+
+function unreachable(cfg) {
+  var tmp = new Set(cfg.nodes);
+  for (var node in reachable(cfg)) {
+    tmp.remove(node)
+  }
+  return tmp;
+}
+
+function buildDominatorTree(cfg) {
+  const tree = {}; // key = node, value = parent
+  for (var node of cfg.postOrder().reverse()) {
+    for (var unreachableNode of unreachable(cfg.remove(node))) {
+      console.error(unreachableNode, ' is unreachable after removing', node);
+      
+    }
+    //tree[node] = ;
+    
+  }
+  console.error(tree)
+  return tree;
 }
 
 module.exports = {
-  ControlFlowGraph: ControlFlowGraph
+  ControlFlowGraph: ControlFlowGraph,
+  buildDominatorTree: buildDominatorTree,
 };
   
