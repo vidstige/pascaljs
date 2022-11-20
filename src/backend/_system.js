63c270fe60135db5152f7209b9e213c0d6f7250d
Samuel Carlsson <samuel.carlsson@gmail.com>
2022-11-20 18:13:30 +0100

    stdlib: Halt

--- src/backend/_system.js
+++ src/backend/_system.js
@@ -1,2 +1,6 @@
 export var __registers = {};
 export function WriteLn() { console.log(Array.prototype.slice.call(arguments).join('')); }
+
+export function Halt(exitcode) {
+    process.exit(exitcode);
+}
