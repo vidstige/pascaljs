f87fb90d64710c5badd538486585d7c6ccdbaf19
Samuel Carlsson <samuel.carlsson@gmail.com>
2022-11-20 18:16:43 +0100

    File functions etc

--- src/backend/_system.js
+++ src/backend/_system.js
@@ -1,6 +1,27 @@
 export var __registers = {};
 export function WriteLn() { console.log(Array.prototype.slice.call(arguments).join('')); }
 
+// System
 export function Halt(exitcode) {
     process.exit(exitcode);
+}
+
+// Strings
+export function Length(s) {
+    return s.length;
+}
+
+export function Byte(char) {
+    return char.charCodeAt(0);
+}
+
+// File I/O
+export function Assign(f, filename) {
+
+}
+export function Reset(f) {
+
+}
+export function Close(f) {
+
 }
